5ce250838f5decebbac44b0b16341bd42223afdb
alexinea <forerunner@outlook.com>
2021-05-09 16:36:32 +0800

    Add PropertyValue accessor.

---
+++ src/Cosmos.Extensions.ObjectVisitors/Cosmos/Reflection/ObjectVisitors/ObjectPropertyValueAccessor.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Reflection;
+
+namespace Cosmos.Reflection.ObjectVisitors
+{
+    public class ObjectPropertyValueAccessor : IPropertyValueAccessor
+    {
+        internal ObjectPropertyValueAccessor(object instance, Type type)
+        {
+            _instance = instance;
+            _type = type;
+        }
+
+        private readonly Type _type;
+        private readonly object _instance;
+
+        public object GetValue(string propertyName)
+        {
+            return ObjectGetter.Type(_type).Instance(_instance).GetValue(propertyName);
+        }
+
+        public object GetValue(string propertyName, BindingFlags bindingAttr)
+        {
+            return ObjectGetter.Type(_type).Instance(_instance).GetValue(propertyName);
+        }
+
+        public void SetValue(string propertyName, object value)
+        {
+            ObjectSetter.Type(_type).Instance(_instance).SetValue(propertyName, value);
+        }
+
+        public void SetValue(string propertyName, BindingFlags bindingAttr, object value)
+        {
+            ObjectSetter.Type(_type).Instance(_instance).SetValue(propertyName, value);
+        }
+
+        public static IPropertyValueAccessor Of<T>(T instance) => new ObjectPropertyValueAccessor<T>(instance);
+
+        public static IPropertyValueAccessor Of(object instance, Type type) => new ObjectPropertyValueAccessor(instance, type);
+
+        public static IPropertyValueAccessor OfStatic<T>() => new StaticPropertyValueAccessor<T>();
+
+        public static IPropertyValueAccessor OfStatic(Type type) => new StaticPropertyValueAccessor(type);
+    }
+
+    public class ObjectPropertyValueAccessor<T> : IPropertyValueAccessor
+    {
+        internal ObjectPropertyValueAccessor(object instance)
+        {
+            _instance = instance;
+        }
+
+        private readonly Type _type = typeof(T);
+        private readonly object _instance;
+
+        public object GetValue(string propertyName)
+        {
+            return ObjectGetter.Type(_type).Instance(_instance).GetValue(propertyName);
+        }
+
+        public object GetValue(string propertyName, BindingFlags bindingAttr)
+        {
+            return ObjectGetter.Type(_type).Instance(_instance).GetValue(propertyName);
+        }
+
+        public void SetValue(string propertyName, object value)
+        {
+            ObjectSetter.Type(_type).Instance(_instance).SetValue(propertyName, value);
+        }
+
+        public void SetValue(string propertyName, BindingFlags bindingAttr, object value)
+        {
+            ObjectSetter.Type(_type).Instance(_instance).SetValue(propertyName, value);
+        }
+    }
+}
---
+++ src/Cosmos.Extensions.ObjectVisitors/Cosmos/Reflection/ObjectVisitors/ObjectPropertyValueGetter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace Cosmos.Reflection.ObjectVisitors
+{
+    public class ObjectPropertyValueGetter : IPropertyValueGetter
+    {
+        internal ObjectPropertyValueGetter() { }
+
+        public object Invoke(Type type, object that, string propertyName)
+        {
+            return ObjectGetter.Type(type).Instance(that).GetValue(propertyName);
+        }
+
+        public object Invoke(Type type, object that, string propertyName, BindingFlags bindingAttr)
+        {
+            return ObjectGetter.Type(type).Instance(that).GetValue(propertyName);
+        }
+
+        public static IPropertyValueGetter Instance { get; } = new ObjectPropertyValueGetter();
+    }
+}
