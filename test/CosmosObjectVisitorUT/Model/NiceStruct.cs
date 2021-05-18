2bf53f5c17280126ca5075c5a44bcdbea26dce29
alexinea <forerunner@outlook.com>
2021-05-18 11:13:26 +0800

    Add a struct model for UT.

---
+++ test/CosmosObjectVisitorUT/Model/NiceStruct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CosmosObjectVisitorUT.Model
+{
+    public struct NiceStruct
+    {
+        public NiceStruct(string name, int age, DateTime birthday, Country country, bool isValid)
+        {
+            Name = name;
+            Age = age;
+            Birthday = birthday;
+            Country = country;
+            IsValid = isValid;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public Country Country { get; set; }
+
+        public bool IsValid { get; set; }
+    }
+}
